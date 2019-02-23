801e06ab06ad922fea37e9da4941649779ec07f4
964494258@qq.com <964494258@qq.com>
2019-02-23 13:42:03 +0800

    测试

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,8 @@
 import request from './request';
 const BASE_URL='http://www.yu6666.top:8080';
 
+
+
 const get=(url,config)=>new Promise((resolve,reject)=>{
   request.get(url,config).then(res=>{
     if(res.data.code===200){
